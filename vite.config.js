7aa40846052770507d2ce9170525a4d645583e4b
Charlie Jiang <w@chariri.moe>
2024-12-07 18:44:46 +0800

    :globe_with_meridians: 进一步完善国际化

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 /** @type {import('vite').UserConfig} */
+
+import { resolve } from 'path';
+
 export default {
   resolve: {
     alias: {
       '../../images/backgrounds': 'uikit/src/images/backgrounds',
       '../../images/components': 'uikit/src/images/components',
       '../../images/icons': 'uikit/src/images/icons'
+    }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        default: resolve(__dirname, 'index.html'),
+        en: resolve(__dirname, 'index-en.html'),
+        ja: resolve(__dirname, 'index-ja.html'),
+      }
     }
   }
 };
